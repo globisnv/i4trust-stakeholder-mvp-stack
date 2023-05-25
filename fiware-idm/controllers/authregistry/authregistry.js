ca64be718dd3bd7028a1269ec715e5eae9e4c651
Jelle <jelle.vandesijpe@gmail.com>
2023-05-25 17:37:45 +0200

    test

--- fiware-idm/controllers/authregistry/authregistry.js
+++ fiware-idm/controllers/authregistry/authregistry.js
@@ -59,7 +59,7 @@ const arrays_are_equal = async function arrays_are_equal(a, b) {
   b.sort();
 
   for (var i = 0; i < a.length; ++i) {
-    if (a[i] !== b[i]) return false;
+    if (a[i] != b[i]) return false;
   }
 
   return true;
