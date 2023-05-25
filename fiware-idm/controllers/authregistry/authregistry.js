6a34845dc6b849d37d353979b3b27003b599a2ae
Jelle <jelle.vandesijpe@gmail.com>
2023-05-25 15:39:30 +0200

    Refined merg functionality (on identifiers instead of types)

--- fiware-idm/controllers/authregistry/authregistry.js
+++ fiware-idm/controllers/authregistry/authregistry.js
@@ -203,7 +203,7 @@ const _upsert_merge_policy = async function _upsert_merge_policy(req, res) {
       }
 
       // Collect type + id combo's per actions and attributes
-      const p_obj_prev_idx = p_types[p_resource.type].findIndex(obj => obj.actions == p_actions && obj.attributes == p_resource.attributes);
+      const p_obj_prev_idx = p_types[p_resource.type].findIndex(obj => obj.actions == p_actions && obj.attrs == p_resource.attributes);
       if (p_obj_prev_idx != -1) {
         p_types[p_resource.type][p_obj_prev_idx].idx.push(p_idx);
         p_types[p_resource.type][p_obj_prev_idx].ids.push(...p_resource.identifiers);
@@ -222,15 +222,28 @@ const _upsert_merge_policy = async function _upsert_merge_policy(req, res) {
       // Policy has the same type as one of the new policies
       if (Object.hasOwn(p_types, p_current_resource.type)) {
 
-        for (let p_current_ids_idx = 0; p_current_ids_idx < p_current_resource.identifiers; p_current_ids_idx++) {
-          if (p_types[p_current_resource.type].some(obj => obj.ids.includes(p_current_resource.identifiers[p_current_ids_idx]))) {
-            p_current_resource.identifiers.splice(p_current_ids_idx, 1);
-            p_current_ids_idx--;
+        // Search whether there is a policy with same actions and same 
+        const p_same_actions_idx = p_types[p_current_resource.type].findIndex(obj => obj.actions == p_current_actions && obj.attrs = p_current_resource.attributes);
+        if (p_same_actions_idx != -1) {
+          for (let p_ids_idx = 0; p_ids_idx < p_types[p_current_resource.type][p_same_actions_idx].ids.length; p_ids_idx++) {
+            const p_id = p_types[p_current_resource.type][p_same_actions_idx].ids[p_ids_idx];
+            if (!p_current_resource.identifiers.includes(p_id)) {
+              p_current_resource.identifiers.push(p_id);
+              p_types[p_current_resource.type][p_same_actions_idx].ids.splice(p_ids_idx, 1);
+              p_ids_idx--;
+            }
+          }
+        }
+        else {
+          for (let p_current_ids_idx = 0; p_current_ids_idx < p_current_resource.identifiers; p_current_ids_idx++) {
+            if (p_types[p_current_resource.type].some(obj => obj.ids.includes(p_current_resource.identifiers[p_current_ids_idx]))) {
+              p_current_resource.identifiers.splice(p_current_ids_idx, 1);
+              p_current_ids_idx--;
+            }
           }
         }
 
         // Remove policy if empty
-        // TODO exceptions to the rule
         if (p_current_resource.identifiers.length == 0) {
           evidence_current.policySets[0].policies.splice(p_current_idx, 1);
         }
